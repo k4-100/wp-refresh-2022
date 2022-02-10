f742b372910a71431412f48f2aaba7cc59ec1900
k4-100 <k4-100@protonmail.com>
2022-02-10 10:26:46 +0100

    Dodano sassa

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-    <?php 
-        get_header();
-        get_footer( );
-    ?>
+<?php 
+    get_header();
+?>  
+
+<h1> <?php the_title(); ?> </h1>
+
+<main>
+<?php 
+    the_content();
+?>
+</main>
+
+
+<?php
+    get_footer();
+?>
