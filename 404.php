c84becc4f44cc8b1f63bc71d625ce206d2d828b8
k4-100 <k4-100@protonmail.com>
2022-02-10 13:48:34 +0100

    Poprawiono strukture theme'u

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php 
+    get_header();
+?> 
+<h1> <?php the_title(); ?> </h1>
+
+<main>
+    <h2>ERROR</h2>
+    <a href="<?= get_site_url()?>">
+        RETURN TO NORMAL
+    </a>
+</main>
+
+
+<?php
+    get_footer();
+?>
---
+++ front-page.php
@@ -0,0 +1,16 @@
+<?php 
+    get_header();
+?>  
+
+<h1> <?php the_title(); ?> </h1>
+<h2> FRONT PAGE </h2>
+<main>
+<?php 
+    the_content();
+?>
+</main>
+
+
+<?php
+    get_footer();
+?>
